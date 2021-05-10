07178b5ab5717b15ba5cab2643476040e9c3e124
Karla Nieves <karlaxnieves@gmail.com>
2021-05-10 16:39:48 -0700

    operator ecvent handler is now working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useReducer} from 'react';
 import reducer, {initialState} from './reducers/index'
-import {addOne, applyNumber} from './actions/index'
+import { applyNumber, changeOperation} from './actions/index'
 
 import './App.css';
 
@@ -11,11 +11,15 @@ function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   // const handleOne = () =>{
-  //   dispatch(addOne(reducer + 1))
+  //   dispatch(addOne(1))
   // }
 
   const handleNumber = (num) => {
     dispatch(applyNumber(num))
+  }
+
+  const handleOperation = (operator) =>{
+    dispatch(changeOperation(operator))
   }
 
 
@@ -44,25 +48,25 @@ function App() {
             <div className="row">
               <CalcButton value={1} onClick={ () => {handleNumber(1)}}/>
               <CalcButton value={2} onClick={ () => {handleNumber(2)}}/>
-              <CalcButton value={3}/>
+              <CalcButton value={3} onClick={ () => {handleNumber(3)}}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={ () => {handleNumber(4)}}/>
+              <CalcButton value={5} onClick={ () => {handleNumber(5)}}/>
+              <CalcButton value={6} onClick={ () => {handleNumber(6)}}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={ () => {handleNumber(7)}}/>
+              <CalcButton value={8} onClick={ () => {handleNumber(8)}}/>
+              <CalcButton value={9} onClick={ () => {handleNumber(9)}}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={"+"} onClick={ () => {handleOperation("+")}}/>
+              <CalcButton value={"*"} onClick={ () => {handleOperation("*")}}/>
+              <CalcButton value={"-"} onClick={ () => {handleOperation("-")}}/>
             </div>
 
             <div className="row ce_button">
--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,10 @@ export const ADD_ONE = "ADD_ONE";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 
+export const changeOperation = (operation) =>{
+    return ({type:CHANGE_OPERATION, payload:operation})
+}
+
 export const addOne = () => {
     return({type:ADD_ONE});
 }
