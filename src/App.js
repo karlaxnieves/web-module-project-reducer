80be13475d7b6150be4b7bdb88d6863c592156b6
Karla Nieves <karlaxnieves@gmail.com>
2021-05-10 15:25:53 -0700

    created a handleOne event handler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useReducer} from 'react';
 import reducer, {initialState} from './reducers/index'
+import {addOne} from './actions/index'
 
 import './App.css';
 
@@ -8,6 +9,10 @@ import CalcButton from './components/CalcButton';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleOne = () =>{
+    dispatch(addOne(reducer + 1))
+  }
 
 
   return (
@@ -33,7 +38,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={handleOne}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
